6bbd5fa2107b0b89a39295edc065f408830aacd8
Paxton <paxton.perry@protonmail.com>
2025-03-30 15:07:01 -0700

    Creating a simple main.tf file

---
+++ main.tf
@@ -0,0 +1,10 @@
+provider "azurerm" {
+    subscription_id = "0dc2ac06-ab33-4bc1-9df3-bc0a61c163f9"
+    features {}
+}
+
+resource "azurerm_resource_group" "example" {
+    name = "rg_tf_access"
+    location = "West US"
+}
+
